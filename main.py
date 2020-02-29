64ba48b4a80460d0e9a7de57f6bfbfb40b7cb2d8
Peter Hurt <peter.hurt@sky.com>
2020-02-29 21:06:27 +0000

    Spawn 200 Cones Test
    Instead of cars

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import cv2
 
 from car import Car
 from scene import Scene
-from simulator.simulator import Simulator
+#from simulator.simulator import Simulator
+from simulator.coneTest import ConeTest
 
 # Set up the basic scene
 scene = Scene(track_path="tracks/brands_hatch.json")
 scene.add_car(Car())
 
 # render scene as an image for NN - Not Carla
-cv2.imshow("", scene.render_track() / 255)
-cv2.waitKey(0)
+#cv2.imshow("", scene.render_track() / 255)
+#cv2.waitKey(0)
 
 
 # Add the scene to the carla simulator
-simulator = Simulator()
+simulator = ConeTest()
 simulator.set_scene(scene)
