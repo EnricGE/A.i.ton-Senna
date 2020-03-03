1b0394f511447b25c1e1ef3d2651b7a143b35b8b
Peter Hurt <peter.hurt@sky.com>
2020-03-03 06:41:17 +0000

    Commenting coneTest.py

--- carla_sim/coneTest.py
+++ carla_sim/coneTest.py
@@ -23,40 +23,42 @@ class ConeTest:
         import random
         number_of_vehicles = 200
         logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-        actor_list = []
+        actor_list = [] #init list of actor IDs
 
         try:
             world = self.__simulator.get_world()
-            spawn_points = world.get_map().get_spawn_points()
+            spawn_points = world.get_map().get_spawn_points() #import list of carla recommended vehicle spawn transforms
             number_of_spawn_points = len(spawn_points)
-            if number_of_vehicles < number_of_spawn_points:
+            if number_of_vehicles < number_of_spawn_points: #randomise which spawn points are used
                 random.shuffle(spawn_points)
-            elif number_of_vehicles > number_of_spawn_points:
+            elif number_of_vehicles > number_of_spawn_points: #hard limit on number of vehicles that can be spawned according to number of spawn points
                 msg = 'requested %d vehicles, but could only find %d spawn points'
                 logging.warning(msg, number_of_vehicles, number_of_spawn_points)
                 number_of_vehicles = number_of_spawn_points
-            # @todo cannot import these directly.
+            
             SpawnActor = carla.command.SpawnActor
-            batch = []
+            batch = [] #init list of spawned actors
 
-            blueprints = world.get_blueprint_library().filter('static.prop.trafficcone02')
-            blueprint = random.choice(blueprints)
-            for cone in self.scene.track.blue_cones:
-                batch.append(SpawnActor(blueprint, carla.Transform(carla.Location(x=cone.point.x, y=cone.point.y))))
+            blueprints = world.get_blueprint_library().filter('static.prop.trafficcone02') #grab single-item list of blueprints
+            blueprint = random.choice(blueprints) #extract single-item from list
+            for cone in self.scene.track.blue_cones: for #for each blue cone in track,
+                batch.append(SpawnActor(blueprint, carla.Transform(carla.Location(x=cone.point.x, y=cone.point.y)))) #spawn cone02 at that location
 
             blueprints = world.get_blueprint_library().filter('static.prop.trafficcone01')
             blueprint = random.choice(blueprints)
-            for cone in self.scene.track.yellow_cones:
-                batch.append(SpawnActor(blueprint, carla.Transform(carla.Location(x=cone.point.x, y=cone.point.y))))
+            for cone in self.scene.track.yellow_cones: #for each yellow cone in track,
+                batch.append(SpawnActor(blueprint, carla.Transform(carla.Location(x=cone.point.x, y=cone.point.y)))) #spawn cone01 at that location
 
             for response in self.__simulator.apply_batch_sync(batch):
                 if response.error:
                     logging.error(response.error)
                 else:
-                    actor_list.append(response.actor_id)
+                    actor_list.append(response.actor_id) #add actor id to list
+
             print('spawned %d cones, press Ctrl+C to exit.' % len(actor_list))
+
             while True:
-                world.wait_for_tick()
+                world.wait_for_tick() #sit around and wait for something to happen
         finally:
             print('\ndestroying %d actors' % len(actor_list))
-            self.__simulator.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
+            self.__simulator.apply_batch([carla.command.DestroyActor(x) for x in actor_list]) #when Ctrl+C'd destroy all spawned actors
